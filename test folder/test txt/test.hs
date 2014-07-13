594e32319accdd5daa77caeb7133e9f45e5d658d
Raquel <raqevill@espol.edu.ec>
2014-07-13 02:59:00 -0500

    lee el txt y hace split y separa ciudad de origen, destino, distncia y precio

--- test folder/test txt/test.hs
+++ test folder/test txt/test.hs
@@ -1,14 +1,14 @@
 import System.IO  
 import Control.Monad
+import Data.List.Split
 
 main = do  
         let list = []
         handle <- openFile "DatasetM.txt" ReadMode
         contents <- hGetContents handle
+        let lineas = splitOneOf ";\n" contents
         let singlewords = words contents
-            list = singlewords
-        print list
+            --list = singlewords
+        --print list
+        print lineas
         hClose handle   
-
-f :: [String] -> [String]
-f = map read
