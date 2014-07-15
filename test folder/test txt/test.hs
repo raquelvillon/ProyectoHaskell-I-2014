63d372e8fcc2f3102b070d9cdd2c70c2c8361454
Raquel <raqevill@espol.edu.ec>
2014-07-15 03:53:47 -0500

    Se encuentran vuelos directos de ciudades predefinidas

--- test folder/test txt/test.hs
+++ test folder/test txt/test.hs
@@ -16,20 +16,38 @@ main= do
         let lineas = splitOneOf ";\n" contents
         --let singlewords = words contents
         --let res= busSec lineas "503"
-        let lista = ["bs","sbs","dvsb","kbj"]
-        let jhj=armarListCom lista
-        print jhj
-        
+        --let lista = ["bs","sbs","dvsb","kbj"]
+        let listaTuplas=armarListCom lineas
+        let coincidencas1=buscarOrg "Dallas, TX" listaTuplas
+        let coincidencas2=buscarDest "San Antonio, TX" coincidencas1
+        --print (length listaTuplas)
+        print  coincidencas2
         hClose handle
 
 
 tomarDos :: [String] -> [String]
-tomarDos l = take 2 l
+tomarDos l = take 4 l
 
 
 armarListCom :: [String]->[[String]]
 armarListCom []=[[]]
-armarListCom l =  [tomarDos l] ++ armarListCom (drop 2 l) 
+armarListCom l =  [tomarDos l] ++ armarListCom (drop 4 l) 
+
+buscarOrg :: String -> [[String]] -> [[String]]
+buscarOrg o [[]] = [[]]
+buscarOrg o itinierarios =  
+    if o `elem` itinierarios!!0
+        then [itinierarios!!0] ++ buscarOrg o (drop 1 itinierarios)
+        else buscarOrg o (drop 1 itinierarios)
+
+buscarDest :: String -> [[String]] -> [[String]]
+buscarDest o [[]] = [[]]
+buscarDest o itinierarios =  
+    if o `elem` itinierarios!!0
+        then [itinierarios!!0] ++ buscarDest o (drop 1 itinierarios)
+        else buscarDest o (drop 1 itinierarios)
+
+
 
 
 busSec::Ord a=>[a]->a->Bool
