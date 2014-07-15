aecc6f66de9c429bffa3a5fdca2b3dc3dcd24495
Raquel <raqevill@espol.edu.ec>
2014-07-15 01:46:23 -0500

    tomar 2 elementos de una lista

--- test folder/test txt/test.hs
+++ test folder/test txt/test.hs
@@ -1,14 +1,32 @@
 import System.IO  
 import Control.Monad
 import Data.List.Split
+data BookInfo = Book Int String [String]
+                deriving (Show)
+data MagazineInfo = Magazine Int String [String]
+                    deriving (Show)
+myInfo = Book 9780135072455 "Algebra of Programming"
+         ["Richard Bird", "Oege de Moor"]
 
-main = do  
-        let list = []
+--lineas = []
+main= do  
+       	--
         handle <- openFile "DatasetM.txt" ReadMode
         contents <- hGetContents handle
         let lineas = splitOneOf ";\n" contents
-        let singlewords = words contents
-            --list = singlewords
-        --print list
-        print lineas
-        hClose handle   
+        --let singlewords = words contents
+        --let res= busSec lineas "503"
+        let lista=tomarDos ["bs","sbs","dvsb"]
+        print lista
+        
+        hClose handle
+
+
+tomarDos :: [String] -> [String]
+tomarDos l = take 2 l
+
+busSec::Ord a=>[a]->a->Bool
+busSec [] _ = False
+busSec (x:xs) ele
+                    | x==ele = True
+                    | True = busSec xs ele
