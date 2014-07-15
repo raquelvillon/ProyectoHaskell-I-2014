2821790f3ea1425a62b3f372bc21b85ec09a82e7
Marcelo Sánchez <marcelo_sanchez_1991@hotmail.com>
2014-07-15 06:53:11 -0500

    Pedir Informacion por teclado

--- test folder/test txt/test.hs
+++ test folder/test txt/test.hs
@@ -1,16 +1,14 @@
 import System.IO  
 import Control.Monad
 import Data.List.Split
-data BookInfo = Book Int String [String]
-                deriving (Show)
-data MagazineInfo = Magazine Int String [String]
-                    deriving (Show)
-myInfo = Book 9780135072455 "Algebra of Programming"
-         ["Richard Bird", "Oege de Moor"]
-
+--data BookInfo = Book Int String [String]
+--                deriving (Show)
+--data MagazineInfo = Magazine Int String [String]
+--                    deriving (Show)
+--myInfo = Book 9780135072455 "Algebra of Programming"
+--         ["Richard Bird", "Oege de Moor"]
 
 main= do  
-       	--
         handle <- openFile "DatasetM.txt" ReadMode
         contents <- hGetContents handle
         let lineas = splitOneOf ";\n" contents
@@ -18,20 +16,30 @@ main= do
         --let res= busSec lineas "503"
         --let lista = ["bs","sbs","dvsb","kbj"]
         let listaTuplas=armarListCom lineas
-        let coincidencas1=buscarOrg "Dallas, TX" listaTuplas
-        let coincidencas2=buscarDest "San Antonio, TX" coincidencas1
+        putStrLn "Ingrese Ciudad Origen: "
+        cOrig <- getLine
+        putStrLn "Ingrese Ciudad Destino: "
+        cDest <- getLine
+        let coincidencas1=buscarOrg cOrig listaTuplas
+        let coincidencas2=buscarDest cDest listaTuplas
+        
+        --let ruta0=armarRuta cOrig listaTuplas
+
+        --let coincidencas1=buscarOrg "Dallas, TX" listaTuplas
+        --let coincidencas2=buscarDest "San Antonio, TX" coincidencas1
         --print (length listaTuplas)
-        print  coincidencas2
+        let impriVal = "Distancia: " ++ coincidencas2 !!0 !!2 ++ "Costo: " ++ coincidencas2 !!0 !!3
+        print  impriVal
+        --print  ruta0
         hClose handle
 
 
-tomarDos :: [String] -> [String]
-tomarDos l = take 4 l
-
+tomarTupla :: [String] -> [String]
+tomarTupla l = take 4 l
 
 armarListCom :: [String]->[[String]]
 armarListCom []=[[]]
-armarListCom l =  [tomarDos l] ++ armarListCom (drop 4 l) 
+armarListCom l =  [tomarTupla l] ++ armarListCom (drop 4 l) 
 
 buscarOrg :: String -> [[String]] -> [[String]]
 buscarOrg o [[]] = [[]]
@@ -43,9 +51,22 @@ buscarOrg o itinierarios =
 buscarDest :: String -> [[String]] -> [[String]]
 buscarDest o [[]] = [[]]
 buscarDest o itinierarios =  
-    if o `elem` itinierarios!!0
-        then [itinierarios!!0] ++ buscarDest o (drop 1 itinierarios)
-        else buscarDest o (drop 1 itinierarios)
+    if o `elem` itinierarios!!1
+        then [itinierarios!!1] ++ buscarDest o (drop 2 itinierarios)
+        else buscarDest o (drop 2 itinierarios)
+
+--mostrarValores :: [[String]] -> return()
+--mostrarValores l = do
+--                    show l !!0 !!0
+--                    show l !!0 !!1
+
+
+armarRuta :: String -> [[String]] -> [[String]]
+armarRuta o [[]] = [[]]
+armarRuta  o itinierarios =  
+    if o `elem` itinierarios!!0 
+        then [itinierarios!!0] ++ buscarDest o (drop 2 itinierarios)
+        else buscarDest o (drop 2 itinierarios)
 
 
 
